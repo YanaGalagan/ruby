5cdc1201fb30539bda184ea717ac5011f5ce71ab
YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
2023-03-04 11:38:28 +0300

    исправлен метод вывода объекта

--- lab2/student.rb
+++ lab2/student.rb
@@ -17,87 +17,63 @@ class Student
 		self.telegram = options[:telegram]
 	end
 
-    #геттеры
-
-    def id
-    	@id
-    end
-
-	def first_name
-		@first_name
-	end
-
-	def middle_name
-		@middle_name
-	end
-
-	def surname
-		@surname
-	end
-
-	def phone_number
-		@phone_number
-	end
-
-	def mail
-		@mail
-	end
-
-	def git
-		@git
-	end
-
-	def telegram
-		@telegram
-	end
-
+    
 	#сеттеры
 	def id=(id_value)
+		raise ArgumentError, "Incorrect value: id=#{id}!" if !id.nil? 
 		@id=id_value
 	end
 
 
 	def first_name=(first_name_value)
+		raise ArgumentError, "Incorrect value: first_name=#{first_name}!" if !first_name.nil? 
 		@first_name=first_name_value
 	end
 
 	def middle_name=(middle_name_value)
+		raise ArgumentError, "Incorrect value: middle_name=#{middle_name}!" if !middle_name.nil? 
 		@middle_name=middle_name_value
 	end
 
 	def surname=(surname_value)
+		raise ArgumentError, "Incorrect value: surname=#{surname}!" if !surname.nil? 
 		@surname=surname_value
 	end
 
 	def phone_number=(phone_number_value)
+		raise ArgumentError, "Incorrect value: phone_number=#{phone_number}!" if !phone_number.nil? 
 		@phone_number=phone_number_value
 	end
 
 	def mail=(mail_value)
+		raise ArgumentError, "Incorrect value: mail=#{mail}!" if !mail.nil? 
 		@mail=mail_value
 	end
 
 	def git=(git_value)
+		raise ArgumentError, "Incorrect value: git=#{git}!" if !git.nil? 
 		@git=git_value
 	end
 
 	def telegram=(telegram_value)
+		raise ArgumentError, "Incorrect value: telegram=#{telegram}!" if !telegram.nil? 
 		@telegram=telegram_value
 	end
 
-	def print_student
-		str = "Information:\n"
-		str += "id: #{self.id}\n" unless self.id.nil?
-		str += "Имя: #{self.first_name}\n"
-		str += "Отчество: #{self.middle_name}\n"
-		str += "Фамилия: #{self.surname}\n"
-		str += "Номер телефона: #{self.phone_number}\n" unless self.phone_number.nil?
-		str += "mail: #{self.mail}\n" unless self.mail.nil?
-		str += "git: #{self.git}\n" unless self.git.nil?
-		str += "telegram: #{self.telegram}\n" unless self.telegram.nil?
-		str += "-------------"
-		str
-	end
+
+	#def self.is_number_phone?(phone_number)
+	#	raise ArgumentError, "#{phone_number} is not number phone" phone_number.class == String or phone_number.nil?
+	#	return true if phone_number=~/^/
+	
+	def to_s
+    result = "#{first_name} #{middle_name} #{surname}"
+    result += " id=#{id}" unless id.nil?
+    result += " phone=#{phone_number}" unless phone_number.nil?
+    result += " git=#{git}" unless git.nil?
+    result += " telegram=#{telegram}" unless telegram.nil?
+    result += " mail=#{mail}" unless mail.nil?
+    result
+  end
 
 
 end
