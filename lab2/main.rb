9dddac833887f27790acd735e02fec2a4124984e
YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
2023-03-06 14:32:05 +0300

    инициализация

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,14 +1,14 @@
 
 require_relative 'student'
 
-student1 = Student.new('Иван', 'Иванович' , 'Иванов',id:1,phone_number:'+79384567891',
-	mail:'ivan555@yandex.ru', git:'github.com/vanSuper', telegram:'Vanusha')
+student1 = Student.new('Иван', 'Иванович' , 'Иванов',id:1,phone_number:'+79285698741',
+	mail:'ivan555@yandex.ru', git:'github.com/vanSuper', telegram:'@Vanusha')
 
 student2 = Student.new('Илья','Александрович','Петров', id:2, phone_number:'+79285698741',
 	mail:'ilisha@mail.ru')
 
 student3= Student.new('Александр', 'Сергеевич','Сидоров', id:3, phone_number:'+79886325417',
-	git:'github.com/superproger',telegram:'Serezha')
+	git:'github.com/superproger',telegram:'@Serezha')
 
 
 puts student1.to_s
