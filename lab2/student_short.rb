4a28fa7dffcda65450ffbc88318824183f09e684
YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
2023-03-09 21:19:07 +0300

    создание класса StudentShort

---
+++ lab2/student_short.rb
@@ -0,0 +1,33 @@
+require 'json'
+
+class StudentShort
+
+	attr_reader :id,:surname,:short_name, :git, :contact
+
+	def initialize(student)
+		@id = student.id
+		@surname = student.surname
+		@short_name = "#{student.first_name[0]}. #{student.middle_name[0]}."
+		@git = student.git unless student.git.nil?
+		@contact = student.get_contact
+	end
+
+	def self.from_str(id,str)
+		res = JSON.parse(str)
+
+		Student_Short.new(Student.new(first_name: res['fisrst_name'], 
+			middle_name:res['middle_name'], surname:res['surname'], id:id,
+			mobile_phone: result['phone'], git: result['git'],
+            mail: result['mail'],telegram: result['telegram']))
+	end
+
+	def to_s
+		res = "#{surname}"
+		res += " #{short_name}"
+        res += " id=#{id}" unless id.nil?
+        res += " git=#{git}" unless git.nil?
+        res += " #{contact}" unless contact.nil?
+        res
+    end
+end
+
