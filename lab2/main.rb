a4b40fdf99108a32e9c2c96bb85fa20acb052d61
YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
2023-03-09 11:19:39 +0300

    обновлён вызов конструктора

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,20 +1,16 @@
 
 require_relative 'student'
 
-student1 = Student.new('Иван', 'Иванович' , 'Иванов',id:1,phone_number:'+79285698741',
+student1 = Student.new(first_name:'Иван', middle_name:'Иванович' ,surname:'Иванов',id:1,phone_number:'+79285698741',
 	mail:'ivan555@yandex.ru', git:'github.com/vanSuper', telegram:'@Vanusha')
 
-student2 = Student.new('Илья','Александрович','Петров', id:2, phone_number:'+79285698741',
+student2 = Student.new(first_name:'Илья',middle_name:'Александрович',surname:'Петров', id:2, phone_number:'+79285698741',
 	mail:'ilisha@mail.ru')
 
-student3= Student.new('Александр', 'Сергеевич','Сидоров', id:3, phone_number:'+79886325417',
-	git:'github.com/superproger',telegram:'@Serezha')
+
 
 student2.set_contacts(telegram: '@petrov2002')
 
-puts Student.pars_str('{"surname": "Сергеев", 
-	"first_name": "Сергей", "middle_name": "Сергеевич", "id": 5, "phone_number": "+79886325417"}')
 
 puts student1.to_s
-puts student2.to_s
-puts student3.to_s
+puts student2.get_info
