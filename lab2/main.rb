bbc5f38f637bd94a293cbbb8c385874f08526c45
YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
2023-03-09 21:20:27 +0300

    тестирование создания объекта нового класса

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,5 +1,6 @@
 
 require_relative 'student'
+require_relative 'student_short'
 
 student1 = Student.new(first_name:'Иван', middle_name:'Иванович' ,surname:'Иванов',id:1,phone_number:'+79285698741',
 	mail:'ivan555@yandex.ru', git:'github.com/vanSuper', telegram:'@Vanusha')
@@ -14,3 +15,6 @@ student2.set_contacts(telegram: '@petrov2002')
 
 puts student1.to_s
 puts student2.get_info
+
+student3 = StudentShort.new(student2)
+puts student3.to_s
