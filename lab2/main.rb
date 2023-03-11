47bb052299cd49eaf86e0c7540f3b433a5cb476f
YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
2023-03-11 09:15:03 +0300

    тест чтения из txt

--- lab2/main.rb
+++ lab2/main.rb
@@ -18,3 +18,9 @@ puts student2.get_info
 
 student3 = StudentShort.new(student2)
 puts student3.to_s
+
+#puts Student.pars_str('{"first_name":"Илья","middle_name":"Александрович",
+#	"surname":"Петров", "id": 2, "phone_number":"+79285698741",
+#	"mail":"ilisha@mail.ru"}')
+
+puts Student.read_from_txt('test_read.txt')
