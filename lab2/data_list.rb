785f0ade66e8abaf6240bf349248dfedcada7f1e
YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
2023-03-17 19:42:50 +0300

    добавлен метод get_selected

--- lab2/data_list.rb
+++ lab2/data_list.rb
@@ -2,15 +2,24 @@
 
 class DataList
   def initialize(objects)
-    self.new_objects = []
+    self.sel_objects = []
     self.list = objects
   end
 
   def select(number)
     raise ArgumentError, "arg 'number' not Integer" if number.class != Integer
-    new_objects.append(number)
+    sel_objects.append(number)
+  end
+
+  def get_selected
+
+    list_id=[]
+    sel_objects.each do |i|
+      list_id.append(list[i].id)
+    end
+    list_id
   end
 
   private
-  attr_accessor :new_objects, :list
+  attr_accessor :sel_objects, :list
 end
