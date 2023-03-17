977a2d01c100560ebf87c5ae5f11914fbfa11d7a
YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
2023-03-17 19:36:49 +0300

    добавлен метод select

--- lab2/data_list.rb
+++ lab2/data_list.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
 
 class DataList
-  def initialize(list)
-    self.list=list
+  def initialize(objects)
+    self.new_objects = []
+    self.list = objects
+  end
+
+  def select(number)
+    raise ArgumentError, "arg 'number' not Integer" if number.class != Integer
+    new_objects.append(number)
   end
 
   private
-  attr_accessor :list
+  attr_accessor :new_objects, :list
 end
