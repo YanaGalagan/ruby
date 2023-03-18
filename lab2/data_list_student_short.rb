ac4719de610c846346ed1461cd737f875fc974d1
YanaGalagan <96586648+YanaGalagan@users.noreply.github.com>
2023-03-18 11:44:41 +0300

    исправила паттерн

--- lab2/data_list_student_short.rb
+++ lab2/data_list_student_short.rb
@@ -12,18 +12,7 @@ class DataListStudentShort < DataList
     ["short_name_initials", "git", "contact"]
   end
 
-  def get_data
-    result = []
-    id = 0
-    list.each do |o|
-      elem = []
-      elem << id
-      # * необходима так как, если нам передадут [1,2,3], то передастся row.push(1,2,3)
-      elem.push(*table_fields(o))
-      result << elem
-      id += 1
-    end
-    DataTable.new(result)
+  protected def table_fields(object)
+    [object.short_name_initials, object.git, object.contact]
   end
-
 end
